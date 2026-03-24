412b6c0b54664e16e0970623e923591bbf84a667
lrrueca <lowellrrueca@gmail.com>
2026-03-25 00:37:05 +0800

    chore: fixing error ts 1434
    - fixing issue identified keyword `use strict` to `'use strict'`

--- db/migrations/20260322051534-create_table.cjs
+++ db/migrations/20260322051534-create_table.cjs
@@ -1,32 +1,32 @@
-use strict;
+'use strict'
 
 /**
  * @type {import('sequelize').Migration}
  */
-async function up (queryInterface, Sequelize) {
-	await queryInterface.createTable('test_table', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		name: {
-			type: Sequelize.STRING,
-			allowNull: false
-		},
-		created_at: {
-			type: Sequelize.DATE,
-			allowNull: false
-		},
-		updated_at: {
-			type: Sequelize.DATE,
-			allowNull: false
-		}
-	});
+async function up(queryInterface, Sequelize) {
+    await queryInterface.createTable('test_table', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        },
+        updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        },
+    })
 }
 
-async function down (queryInterface, Sequelize) {
-	await queryInterface.dropTable('test_table');
+async function down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('test_table')
 }
 
-module.exports = { up, down };
+module.exports = { up, down }
