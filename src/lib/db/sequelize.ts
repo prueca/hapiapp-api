cce5c957b17c6ea1b61beaa2a74b321a5ae04e26
Lowell Rueca <lowellrrueca@gmail.com>
2026-03-26 23:48:31 +0800

    Update sequelize.ts to add underscored: true option

--- src/lib/db/sequelize.ts
+++ src/lib/db/sequelize.ts
@@ -3,6 +3,9 @@ import { Sequelize } from 'sequelize'
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: String(process.env.DB),
+    define: {
+        underscored: true,
+    },
 })
 
 sequelize.sync()
