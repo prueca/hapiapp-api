3a8d50545d767b3a4b5a25b04aad54b5c7670b51
Lowell Rueca <lowellrrueca@gmail.com>
2026-03-28 07:10:20 +0800

    Refactor userCreate function signature formatting

--- src/lib/domains/user/userCreate.ts
+++ src/lib/domains/user/userCreate.ts
@@ -2,16 +2,14 @@ import User from '@/lib/db/users'
 import { Model } from 'sequelize'
 import bcrypt from 'bcryptjs'
 
-export default async function userCreate(
-    userData: {
-        firstName: string
-        middleName?: string
-        lastName: string
-        username: string
-        password: string
-        role: string
-    },
-): Promise<Model<any, any>> {
+export default async function userCreate(userData: {
+    firstName: string
+    middleName?: string
+    lastName: string
+    username: string
+    password: string
+    role: string
+}): Promise<Model<any, any>> {
     // Hash password
     const hashedPassword = await bcrypt.hash(userData.password, 10)
 
