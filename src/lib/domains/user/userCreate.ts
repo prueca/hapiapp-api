2ffc9666d646c9cd6b624283876cc72435ef5dac
Lowell Rueca <lowellrrueca@gmail.com>
2026-03-28 06:29:36 +0800

    Add userCreate domain function

---
+++ src/lib/domains/user/userCreate.ts
@@ -0,0 +1,29 @@
+import User from '@/lib/db/users'
+import { Model } from 'sequelize'
+import bcrypt from 'bcryptjs'
+
+export default async function userCreate(
+    userData: {
+        firstName: string
+        middleName?: string
+        lastName: string
+        username: string
+        password: string
+        role: string
+    },
+): Promise<Model<any, any>> {
+    // Hash password
+    const hashedPassword = await bcrypt.hash(userData.password, 10)
+
+    // Create user
+    const user = await User.create({
+        firstName: userData.firstName,
+        middleName: userData.middleName || '',
+        lastName: userData.lastName,
+        username: userData.username,
+        password: hashedPassword,
+        role: userData.role,
+    })
+
+    return user
+}
