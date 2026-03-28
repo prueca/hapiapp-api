0f353a07de1b4343478843972982ef7642d1667c
Lowell Rueca <lowellrrueca@gmail.com>
2026-03-28 10:42:43 +0800

    Add /users route with userGetAll handler

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import Context from '@/lib/context'
 import ping from '@/handler/ping'
+import userGetAll from '@/handler/user/userGetAll.handler'
 import middleware from '@/middleware/middleware'
 
 const router = Router()
 
 router.get('/ping', Context.middleware(middleware), Context.handler(ping))
+
+// api: users
+router.get('/users', Context.handler(userGetAll))
 
 export default router
