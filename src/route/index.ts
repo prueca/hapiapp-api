37e6e8105362ef71a0b188e0a8eb1f8866632a77
Lowell Rueca <lowellrrueca@gmail.com>
2026-03-29 03:56:31 +0800

    feat: add POST /api/users route

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import Context from '@/lib/context'
 import ping from '@/handler/ping'
+import userCreate from '@/handler/user/userCreate.handler'
 import userGetAll from '@/handler/user/userGetAll.handler'
 import userGetById from '@/handler/user/userGetById.handler'
 
@@ -13,5 +14,6 @@ router.get('/ping', Context.middleware(middleware), Context.handler(ping))
 // api: users
 router.get('/api/users', Context.handler(userGetAll))
 router.get('/api/users/:id', Context.handler(userGetById))
+router.post('/api/users', Context.handler(userCreate))
 
 export default router
