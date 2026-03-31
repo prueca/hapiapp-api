1ee7922a95c0ae7386aad8f15fbb40a8e3cdcc5d
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-01 01:27:18 +0800

    feat: add userRole routes to index

--- src/route/index.ts
+++ src/route/index.ts
@@ -7,6 +7,12 @@ import userGetById from '@/handler/user/userGetById.handler'
 import userUpdate from '@/handler/user/userUpdate.handler'
 import userDelete from '@/handler/user/userDelete.handler'
 
+import userRoleGetAllHandler from '@/handler/userRole/userRoleGetAll.handler'
+import userRoleGetByIdHandler from '@/handler/userRole/userRoleGetById.handler'
+import userRoleCreateHandler from '@/handler/userRole/userRoleCreate.handler'
+import userRoleUpdateHandler from '@/handler/userRole/userRoleUpdate.handler'
+import userRoleDeleteHandler from '@/handler/userRole/userRoleDelete.handler'
+
 import middleware from '@/middleware/middleware'
 
 const router = Router()
@@ -19,5 +25,12 @@ router.get('/api/users/:id', Context.handler(userGetById))
 router.post('/api/users/:id', Context.handler(userCreate))
 router.put('/api/users/:id', Context.handler(userUpdate))
 router.delete('/api/users/:id', Context.handler(userDelete))
+
+// api: user_roles
+router.get('/api/user_role', Context.handler(userRoleGetAllHandler))
+router.get('/api/use_role/:id', Context.handler(userRoleGetByIdHandler))
+router.post('/api/user_role/:id', Context.handler(userRoleCreateHandler))
+router.put('/api/user_role/:id', Context.handler(userRoleUpdateHandler))
+router.delete('/api/use_role/:id', Context.handler(userRoleDeleteHandler))
 
 export default router
