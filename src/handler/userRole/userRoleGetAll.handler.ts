86d31179b6558cf10a4ff4389098283fefdd7bd2
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-06 23:25:16 +0800

    feat: add role filter parameter to userRoleGetAll handler

--- src/handler/userRole/userRoleGetAll.handler.ts
+++ src/handler/userRole/userRoleGetAll.handler.ts
@@ -2,13 +2,14 @@ import Context from '@/lib/context'
 import userRoleGetAll from '@/lib/domains/userRole/userRoleGetAll'
 
 export default async (ctx: Context) => {
-    const { limit, after, sortBy, sortOrder } = ctx.params
+    const { limit, after, sortBy, sortOrder, role } = ctx.params
 
     const userRoles = await userRoleGetAll(
         Number(limit) || 10,
         (after as string) || null,
         (sortBy as string) || 'createdAt',
         (sortOrder as 'ASC' | 'DESC') || 'DESC',
+        (role as string) || null,
     )
 
     ctx.send({ data: userRoles }, 200)
