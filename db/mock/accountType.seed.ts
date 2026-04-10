1e6553e2374b1bd2e907b6a18f03278314853539
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-10 23:54:20 +0800

    fix: correct bulkCreate syntax in accountType.seed.ts

--- db/mock/accountType.seed.ts
+++ db/mock/accountType.seed.ts
@@ -4,9 +4,10 @@ import { mockAccountTypes } from './accountType.mock'
 
 export default async function () {
     try {
-        await AccountType.bulkCreate(mockAccountTypes, {
-            transaction: await sequelize.transaction(),
-        })
+        ;(await AccountType.bulkCreate(mockAccountTypes),
+            {
+                transaction: await sequelize.transaction(),
+            })
         console.log('Account type data seeded successfully.')
     } catch (error) {
         console.error('Seed failed:', error)
