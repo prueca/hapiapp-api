bb809345b42cf24bd4579967c756f0dfe421ffca
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-18 12:30:53 +0800

    feat: add UserRole to database models

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,8 +1,10 @@
 import FreezerType from './FreezerType'
+import UserRole from './UserRole'
 import sequelize from './sequelize'
 
 const models = {
     FreezerType,
+    UserRole,
 }
 
 export { sequelize }
