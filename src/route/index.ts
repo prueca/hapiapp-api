c8403aaaf1b5b57d402066d86dba80c2e07e8d8d
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-18 12:31:19 +0800

    feat: add userRole routes

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import freezerType from '@/handler/freezerType'
+import userRole from '@/handler/userRole'
 
 const routes = new Hono()
 
@@ -9,5 +10,12 @@ routes.get('/freezer_types/:id', freezerType.getById)
 routes.post('/freezer_types/search', freezerType.search)
 routes.put('/freezer_types/:id', freezerType.update)
 routes.delete('/freezer_types/:id', freezerType.delete)
+
+// User Role
+routes.post('/user_roles', userRole.create)
+routes.get('/user_roles/:id', userRole.getById)
+routes.post('/user_roles/search', userRole.search)
+routes.put('/user_roles/:id', userRole.update)
+routes.delete('/user_roles/:id', userRole.delete)
 
 export default routes
