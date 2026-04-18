6fac3bb9f9e1763c3beaa8462ce71c237689b9d8
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-18 12:08:36 +0800

    feat: add UserRole Sequelize model

---
+++ src/lib/db/UserRole.ts
@@ -0,0 +1,35 @@
+import sequelize from './sequelize'
+import { DataTypes, Model } from 'sequelize'
+import ulid from '@/lib/util/ulid'
+
+const attributes = {
+    id: {
+        ...ulid.attr(),
+        primaryKey: true,
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+}
+
+const options = {
+    sequelize,
+    freezeTableName: true,
+    tableName: 'user_role',
+    timestamps: true,
+    indexes: [
+        {
+            unique: true,
+            fields: ['role'],
+        },
+    ],
+}
+
+class UserRole extends Model {}
+
+UserRole.init(attributes, options)
+UserRole.sync()
+
+export default UserRole
