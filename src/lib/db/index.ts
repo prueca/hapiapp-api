62795ac4580e8b1dae8037d4ceecb4f8ebf175e6
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-19 11:12:04 +0800

    feat: export User model in database index

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,12 @@
 import FreezerType from './FreezerType'
 import UserRole from './UserRole'
+import User from './User'
 import sequelize from './sequelize'
 
 const models = {
     FreezerType,
     UserRole,
+    User,
 }
 
 export { sequelize }
