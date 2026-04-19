305f38e6038db36b033508a2e19c95ad4fb251d7
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-19 11:35:26 +0800

    feat: add user UPDATE handler

---
+++ src/handler/user/user.update.ts
@@ -0,0 +1,66 @@
+import type { Context } from 'hono'
+import { HTTPException } from 'hono/http-exception'
+import { StatusCodes, ReasonPhrases } from 'http-status-codes'
+import db from '@/lib/db'
+import z from 'zod'
+import _ from 'lodash'
+
+const schema = z.object({
+    firstName: z.string().nonempty().optional(),
+    middleName: z.string().nonempty().optional(),
+    lastName: z.string().nonempty().optional(),
+    username: z.string().nonempty().optional(),
+    password: z.string().nonempty().optional(),
+    role: z.string().nonempty().optional(),
+    accountId: z.string().nonempty().optional(),
+})
+
+export default async (c: Context) => {
+    const body = await c.req.json()
+    const parsed = schema.safeParse(body)
+
+    if (!parsed.success) {
+        throw new HTTPException(StatusCodes.BAD_REQUEST, {
+            message: ReasonPhrases.BAD_REQUEST,
+            cause: parsed.error.issues,
+        })
+    }
+
+    const id = c.req.param('id')!
+    const record = await db.User.findByPk(id)
+
+    if (!record) {
+        throw new HTTPException(StatusCodes.NOT_FOUND, {
+            message: ReasonPhrases.NOT_FOUND,
+        })
+    }
+
+    try {
+        const changes = _.pick(parsed.data, [
+            'firstName',
+            'middleName',
+            'lastName',
+            'username',
+            'password',
+            'role',
+            'accountId',
+        ])
+
+        return c.json({
+            data: await record.update(changes),
+        })
+    } catch (err: any) {
+        switch (err.name) {
+            case 'SequelizeUniqueConstraintError':
+                throw new HTTPException(StatusCodes.CONFLICT, {
+                    message: ReasonPhrases.CONFLICT,
+                    cause: err.name,
+                })
+            default:
+                throw new HTTPException(StatusCodes.INTERNAL_SERVER_ERROR, {
+                    message: err.message,
+                    cause: err.stack,
+                })
+        }
+    }
+}
