2ebdf04ee03d1ecc1b81a5176f4102cbb2829792
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-23 01:17:14 +0800

    feat: add account type POST create endpoint

---
+++ src/handler/accountType/accountType.create.ts
@@ -0,0 +1,45 @@
+import type { Context } from 'hono'
+import { HTTPException } from 'hono/http-exception'
+import { StatusCodes, ReasonPhrases } from 'http-status-codes'
+import db from '@/lib/db'
+import z from 'zod'
+
+const schema = z.object({
+    type: z.string().nonempty(),
+})
+
+export default async (c: Context) => {
+    const body = await c.req.json()
+    const parsed = schema.safeParse(body)
+
+    if (!parsed.success) {
+        throw new HTTPException(StatusCodes.BAD_REQUEST, {
+            message: ReasonPhrases.BAD_REQUEST,
+            cause: parsed.error.issues,
+        })
+    }
+
+    try {
+        return c.json({
+            data: await db.AccountType.create(parsed.data),
+        })
+    } catch (err: any) {
+        switch (err.name) {
+            case 'SequelizeUniqueConstraintError':
+                throw new HTTPException(StatusCodes.CONFLICT, {
+                    message: ReasonPhrases.CONFLICT,
+                    cause: err.name,
+                })
+            case 'SequelizeValidationError':
+                throw new HTTPException(StatusCodes.UNPROCESSABLE_ENTITY, {
+                    message: ReasonPhrases.UNPROCESSABLE_ENTITY,
+                    cause: err.name,
+                })
+            default:
+                throw new HTTPException(StatusCodes.INTERNAL_SERVER_ERROR, {
+                    message: err.message,
+                    cause: err.stack,
+                })
+        }
+    }
+}
