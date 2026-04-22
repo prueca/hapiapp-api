2f0a27c1444ddcad415e794ec2c4cade7b8fcbee
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-23 01:20:56 +0800

    feat: add account type PUT update endpoint

---
+++ src/handler/accountType/accountType.update.ts
@@ -0,0 +1,52 @@
+import type { Context } from 'hono'
+import { HTTPException } from 'hono/http-exception'
+import { StatusCodes, ReasonPhrases } from 'http-status-codes'
+import db from '@/lib/db'
+import z from 'zod'
+import _ from 'lodash'
+
+const schema = z.object({
+    type: z.string().nonempty().optional(),
+})
+
+export default async (c: Context) => {
+    const body = await c.req.json()
+    const parsed = schema.safeParse(body)
+
+    if (!parsed.success) {
+        throw new HTTPException(StatusCodes.BAD_REQUEST, {
+            message: ReasonPhrases.BAD_REQUEST,
+            cause: parsed.error.issues,
+        })
+    }
+
+    const id = c.req.param('id')!
+    const record = await db.AccountType.findByPk(id)
+
+    if (!record) {
+        throw new HTTPException(StatusCodes.NOT_FOUND, {
+            message: ReasonPhrases.NOT_FOUND,
+        })
+    }
+
+    try {
+        const changes = _.pick(parsed.data, ['type'])
+
+        return c.json({
+            data: await record.update(changes),
+        })
+    } catch (err: any) {
+        switch (err.name) {
+            case 'SequelizeUniqueConstraintError':
+                throw new HTTPException(StatusCodes.CONFLICT, {
+                    message: ReasonPhrases.CONFLICT,
+                    cause: err.name,
+                })
+            default:
+                throw new HTTPException(StatusCodes.INTERNAL_SERVER_ERROR, {
+                    message: err.message,
+                    cause: err.stack,
+                })
+        }
+    }
+}
