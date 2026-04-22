959048dfea79a790e5a0b7a9a2cef8758b9c69ee
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-23 00:54:47 +0800

    feat: add AccountType Sequelize model

---
+++ src/lib/db/AccountType.ts
@@ -0,0 +1,35 @@
+import sequelize from './sequelize'
+import { DataTypes, Model } from 'sequelize'
+import ulid from '@/lib/util/ulid'
+
+const attributes = {
+    id: {
+        ...ulid.attr(),
+        primaryKey: true,
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+}
+
+const options = {
+    sequelize,
+    freezeTableName: true,
+    tableName: 'account_type',
+    timestamps: true,
+    indexes: [
+        {
+            unique: true,
+            fields: ['type'],
+        },
+    ],
+}
+
+class AccountType extends Model {}
+
+AccountType.init(attributes, options)
+AccountType.sync()
+
+export default AccountType
