db3549335442e5f221e53c52d924438e7b2f47e2
Peter John Rueca <peter.rueca@safetychampion.online>
2026-04-22 11:29:57 +0800

    fix: catch populate errors. allow to override options in 'ulid.attr'

--- db/populate.ts
+++ db/populate.ts
@@ -3,6 +3,7 @@ import { parseArgs } from 'node:util'
 import assert from 'assert'
 import db from '@/lib/db'
 import csv from 'csvtojson'
+import _ from 'lodash'
 
 const { values } = parseArgs({
     options: {
@@ -24,10 +25,26 @@ assert.ok(data, 'Data not provided')
 
 const rows = await csv().fromFile(data)
 const model = db[entity as keyof typeof db]
+let row: any
 
-for (const row of rows) {
-    console.log(row)
-    await model.upsert(row)
+await model.sync()
+
+try {
+    for (row of rows) {
+        const sanitized = _.mapValues(row, (v) => (v === '' ? null : v))
+        await model.upsert(sanitized)
+    }
+
+    console.log(`Table '${model.tableName}' populated successfully.`)
+} catch (err: any) {
+    switch (err.name) {
+        case 'SequelizeUniqueConstraintError':
+        case 'SequelizeValidationError':
+            console.log(row)
+            console.error(`${err.name}: ${err.message}`)
+            break
+
+        default:
+            throw err
+    }
 }
-
-console.log(`Table '${model.tableName}' populated successfully.`)
--- src/lib/util/ulid.ts
+++ src/lib/util/ulid.ts
@@ -4,7 +4,7 @@ import { DataTypes } from 'sequelize'
 const ulid = monotonicFactory()
 const generate = () => ulid()
 
-const attr = () => {
+const attr = ({ allowNull = false, primaryKey = false, ...options } = {}) => {
     // We use this function to define id attributes for our models.
     // This fixes the issue of id fields not being created because
     // of the same reference when we define multiple fields with
@@ -16,11 +16,15 @@ const attr = () => {
         defaultValue: generate,
         validate: {
             isULID(value: string) {
+                if (allowNull && value === null) {
+                    return
+                }
                 if (!isValid(value)) {
                     throw new Error('Invalid Id')
                 }
             },
         },
+        ...options,
     }
 }
 
