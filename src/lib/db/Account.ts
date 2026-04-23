30d68d402df7ce52b9733513fe68e244b786589e
Lowell Rueca <lowellrrueca@gmail.com>
2026-04-23 23:55:30 +0800

    Add Account Sequelize model with required fields

---
+++ src/lib/db/Account.ts
@@ -0,0 +1,52 @@
+import sequelize from './sequelize'
+import { DataTypes, Model } from 'sequelize'
+import ulid from '@/lib/util/ulid'
+
+const attributes = {
+    id: ulid.attr({
+        primaryKey: true,
+    }),
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    isrCode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    sapCode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    companyCode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    accountTypeId: ulid.attr({
+        allowNull: false,
+    }),
+    associateId: ulid.attr({
+        allowNull: true,
+    }),
+}
+
+const options = {
+    sequelize,
+    freezeTableName: true,
+    tableName: 'account',
+    timestamps: true,
+}
+
+class Account extends Model {}
+
+Account.init(attributes, options)
+
+export default Account
