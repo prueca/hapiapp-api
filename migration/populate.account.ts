f6a3a1a394f916cd1740af724e4d3bbaf7417a71
Peter John Rueca <peter.rueca@safetychampion.online>
2026-08-14 00:28:49 +0800

    account migration script v3

--- migration/populate.account.ts
+++ migration/populate.account.ts
@@ -1,22 +1,107 @@
-import db from '@/lib/db'
+import 'dotenv/config'
 import _ from 'lodash'
+import z from 'zod'
+import { Op } from 'sequelize'
 
-export default async (mock: Record<string, any>[]) => {
-    for (const item of mock) {
-        let record: Record<string, any> = _.pick(item, [
-            'id',
-            'type',
-            'name',
-            'address',
-            'phone',
-            'isr_code',
-            'sap_code',
-            'company_code',
-            'associate_id',
-        ])
+import db, { sequelize } from '../src/lib/db'
+import ulid from '../src/lib/util/ulid'
+import accountTypes from '../src/lib/config/account.types'
 
-        record = _.mapKeys(record, (v, k) => _.camelCase(k))
+const FRESH = true
+const SOURCE = './mock/accounts.json'
 
-        await db.Account.create(record)
+type PlainObject = {
+    [key: string]: any
+}
+
+const main = async () => {
+    const txn = await sequelize.transaction()
+
+    try {
+        await db.Account.sync({ force: FRESH })
+
+        const schema = z
+            .object({
+                id: z.ulid(),
+                type: z.enum([
+                    accountTypes.DISTRIBUTOR,
+                    accountTypes.DEALER,
+                    accountTypes.FRANCHISEE,
+                ]),
+                name: z.string().nonempty(),
+                address: z.string().nonempty(),
+                phone: z.string().nonempty(),
+                isrCode: z.string().nonempty(),
+                sapCode: z.string().nonempty(),
+                companyCode: z.string().nonempty(),
+                associateId: z.ulid().or(z.null()),
+            })
+            .refine((data) => {
+                if (data.type === accountTypes.DISTRIBUTOR) {
+                    return data.associateId === null
+                }
+
+                return (
+                    data.associateId !== null && ulid.isValid(data.associateId)
+                )
+            })
+
+        /**
+         * Populate table
+         */
+
+        const mock: PlainObject[] = (await import(SOURCE)).default
+
+        let accounts: PlainObject[] = _.map(mock, (item) => {
+            let record: PlainObject = _.pick(item, [
+                'id',
+                'type',
+                'name',
+                'address',
+                'phone',
+                'isr_code',
+                'sap_code',
+                'company_code',
+                'associate_id',
+            ])
+
+            record = _.mapKeys(record, (v, k) => _.camelCase(k))
+            record = schema.parse(record)
+
+            return record
+        })
+
+        accounts = await db.Account.bulkCreate(accounts, { transaction: txn })
+
+        /**
+         * Check if parent exists
+         */
+
+        db.Account.belongsTo(db.Account, {
+            as: 'parent',
+            foreignKey: 'associateId',
+        })
+
+        const orphaned = await db.Account.findAll({
+            include: 'parent',
+            where: {
+                associateId: { [Op.ne]: null },
+                '$parent.id$': null,
+            },
+            transaction: txn,
+            raw: true,
+        })
+
+        if (orphaned.length) {
+            throw new Error(`${orphaned.length} orphaned accounts found`)
+        }
+
+        await txn.commit()
+    } catch (e: any) {
+        await txn.rollback()
+
+        console.log(e.message)
     }
 }
+
+main()
