e4f591af6552384fc3948c34a8f9b72d60feea87
Peter John Rueca <peter.rueca@safetychampion.online>
2026-08-16 11:05:40 +0800

    auth niddleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,47 @@
+import { Context, Next } from 'hono'
+import { getCookie } from 'hono/cookie'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
+import jwt from 'jsonwebtoken'
+import _ from 'lodash'
+
+import User from '@/lib/db/User'
+import Account from '@/lib/db/Account'
+
+export default async (c: Context, next: Next) => {
+    const accessToken = getCookie(c, 'access-token')
+
+    if (!accessToken) {
+        return c.json(
+            {
+                message: ReasonPhrases.UNAUTHORIZED,
+            },
+            StatusCodes.UNAUTHORIZED,
+        )
+    }
+
+    try {
+        type TokenPayload = {
+            user: User
+            account: Account
+        }
+
+        let payload = jwt.verify(
+            accessToken,
+            process.env.ACCESS_TOKEN_SECRET as string,
+        ) as TokenPayload
+
+        payload = _.pick(payload, ['user', 'account'])
+
+        c.set('user', payload.user)
+        c.set('account', payload.account)
+
+        await next()
+    } catch {
+        return c.json(
+            {
+                message: ReasonPhrases.UNAUTHORIZED,
+            },
+            StatusCodes.UNAUTHORIZED,
+        )
+    }
+}
