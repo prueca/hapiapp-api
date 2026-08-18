3c3e110cc0a49b01b3546c7962438b36c4ec1e10
Lowell Rueca <lowellrrueca@gmail.com>
2026-08-18 22:59:24 +0800

    feat: add logout route

--- src/route/index.ts
+++ src/route/index.ts
@@ -68,6 +68,7 @@ routes.put('/users/:id', user.update)
 routes.delete('/users/:id', user.delete)
 routes.post('/users/login', user.login)
 routes.post('/users/authenticate', user.auth)
+routes.post('/users/logout', user.logout)
 routes.post('/users/whoami', user.whoami)
 
 export default routes
